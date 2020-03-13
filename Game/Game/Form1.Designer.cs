0497b5491603fa843786ab7a0d222461f18bf240
VictorVel8 <victorvel801@gmail.com>
2020-03-13 16:43:42 +0200

    First Commit

---
+++ Game/Game/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.turn = new System.Windows.Forms.Label();
+            this.resetbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(776, 38);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Coloana1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(776, 82);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Coloana2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(776, 131);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Coloana3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(776, 179);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 23);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "Coloana4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(776, 229);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 23);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "Coloana5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(776, 279);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 23);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "Coloana6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(776, 332);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 23);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "Colona7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // turn
+            // 
+            this.turn.AutoSize = true;
+            this.turn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turn.Location = new System.Drawing.Point(394, 413);
+            this.turn.Name = "turn";
+            this.turn.Size = new System.Drawing.Size(197, 25);
+            this.turn.TabIndex = 7;
+            this.turn.Text = "Tura jucatorului 1";
+            this.turn.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // resetbtn
+            // 
+            this.resetbtn.Location = new System.Drawing.Point(46, 427);
+            this.resetbtn.Name = "resetbtn";
+            this.resetbtn.Size = new System.Drawing.Size(149, 47);
+            this.resetbtn.TabIndex = 8;
+            this.resetbtn.Text = "Reset game";
+            this.resetbtn.UseVisualStyleBackColor = true;
+            this.resetbtn.Click += new System.EventHandler(this.resetbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 528);
+            this.Controls.Add(this.resetbtn);
+            this.Controls.Add(this.turn);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Label turn;
+        private System.Windows.Forms.Button resetbtn;
+    }
+}
+
---
+++ Game/Game/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+
+        private Rectangle[] coloane;
+        private matrice_clasa tabla_joc;
+        private int tura_jucator;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.coloane = new Rectangle[7];
+            this.tabla_joc = new matrice_clasa();
+            tura_jucator = 0;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.BlueViolet, 24, 24, 340, 300);
+            for (int i=0; i<6; ++i)
+            {
+                for (int j=0; j<7; ++j)
+                {
+                    if (i == 0)
+                        this.coloane[j] = new Rectangle(32 + 48 * j, 24, 32, 300);
+                    tabla_joc.desenare_cerc(i, j, e.Graphics, 0);
+                  //  umplere_cerc(i, j, e.Graphics, 0);
+                }
+            }
+        }
+
+        private void umplere_cerc(int i, int j, Graphics e, int culoare)
+        {
+           
+            e.FillEllipse(Brushes.White, 32 + 48 * j, 32 + 48 * i, 32, 32);
+        }
+
+        private int cautare_linie(int j)
+        {
+            int last = -1;
+            for (int i=0; i<6; ++i)
+            {
+                if (tabla_joc.getMat(i,j)==0)
+                {
+                    last = i;
+                }
+            }
+            return last;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            int linie_disponibila = cautare_linie(0);
+            int coloana = 0;
+            if (linie_disponibila == -1)
+                MessageBox.Show("Linia este completa. Incearca pe alta linie");
+            else
+            {
+                if (tura_jucator % 2 == 0)
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 1);
+                else
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 2);
+                tura_jucator++;
+                if (tura_jucator % 2 == 0)
+                    turn.Text = "Tura jucatorului 1";
+                else
+                    turn.Text = "Tura jucatorului 2";
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            int linie_disponibila = cautare_linie(1);
+            if (linie_disponibila == -1)
+                MessageBox.Show("Linia este completa. Incearca pe alta linie");
+            else
+            {
+                int coloana = 1;
+                if (tura_jucator % 2 == 0)
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 1);
+                else
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 2);
+                tura_jucator++;
+                if (tura_jucator % 2 == 0)
+                    turn.Text = "Tura jucatorului 1";
+                else
+                    turn.Text = "Tura jucatorului 2";
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            int linie_disponibila = cautare_linie(2);
+            int coloana = 2;
+            if (linie_disponibila == -1)
+                MessageBox.Show("Linia este completa. Incearca pe alta linie");
+            else
+            {
+                if (tura_jucator % 2 == 0)
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 1);
+                else
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 2);
+                tura_jucator++;
+                if (tura_jucator % 2 == 0)
+                    turn.Text = "Tura jucatorului 1";
+                else
+                    turn.Text = "Tura jucatorului 2";
+            }
+            }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            int linie_disponibila = cautare_linie(3);
+            int coloana = 3;
+            if (linie_disponibila == -1)
+                MessageBox.Show("Linia este completa. Incearca pe alta linie");
+            else
+            {
+                if (tura_jucator % 2 == 0)
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 1);
+                else
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 2);
+                tura_jucator++;
+                if (tura_jucator % 2 == 0)
+                    turn.Text = "Tura jucatorului 1";
+                else
+                    turn.Text = "Tura jucatorului 2";
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            int linie_disponibila = cautare_linie(4);
+            int coloana = 4;
+            if (linie_disponibila == -1)
+                MessageBox.Show("Linia este completa. Incearca pe alta linie");
+            else
+            {
+                if (tura_jucator % 2 == 0)
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 1);
+                else
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 2);
+                tura_jucator++;
+                if (tura_jucator % 2 == 0)
+                    turn.Text = "Tura jucatorului 1";
+                else
+                    turn.Text = "Tura jucatorului 2";
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            int linie_disponibila = cautare_linie(5);
+            int coloana = 5;
+            if (linie_disponibila == -1)
+                MessageBox.Show("Linia este completa. Incearca pe alta linie");
+            else
+            {
+                if (tura_jucator % 2 == 0)
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 1);
+                else
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 2);
+                tura_jucator++;
+                if (tura_jucator % 2 == 0)
+                    turn.Text = "Tura jucatorului 1";
+                else
+                    turn.Text = "Tura jucatorului 2";
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            int linie_disponibila = cautare_linie(6);
+            int coloana = 6;
+            if (linie_disponibila == -1)
+                MessageBox.Show("Linia este completa. Incearca pe alta linie");
+            else
+            {
+                if (tura_jucator % 2 == 0)
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 1);
+                else
+                    tabla_joc.desenare_cerc(linie_disponibila, coloana, this.CreateGraphics(), 2);
+                tura_jucator++;
+                if (tura_jucator % 2 == 0)
+                    turn.Text = "Tura jucatorului 1";
+                else
+                    turn.Text = "Tura jucatorului 2";
+            }
+        }
+
+        private void resetbtn_Click(object sender, EventArgs e)
+        {
+            tabla_joc.reset_game();
+            tura_jucator = 0;
+            turn.Text = "Tura jucatorului 1";
+            for (int i=0; i<6; ++i)
+            {
+                for (int j=0; j<7; ++j)
+                {
+                    tabla_joc.desenare_cerc(i, j, this.CreateGraphics(), 0);
+                }
+            }
+        }
+    }
+
+    public class matrice_clasa
+    {
+        private int[,] mat = new int[6, 7];
+
+        public int getMat(int i, int j)
+        {
+            return mat[i, j];
+        }
+
+        public void setMat(int i, int j, int val)
+        {
+            mat[i, j] = val;
+        }
+
+        public matrice_clasa()
+        {
+            for (int i=0; i<6; ++i)
+            {
+                for (int j = 0; j < 7; ++j)
+                    mat[i,j] = 0;
+            }
+        }
+
+        bool verificare_linie(int l, int c, int val)
+        {
+            int nr = 0;
+            int auxc = c;
+            while(auxc<7 && mat[l,auxc]==val)
+            {
+                auxc++;
+                nr++;
+            }
+            auxc = c - 1;
+            while(auxc>=0 && mat[l,auxc]==val)
+            {
+                auxc--;
+                nr++;
+            }
+            if (nr >= 4)
+                return true;
+            return false;
+        }
+
+        bool verificare_coloana(int l, int c, int val)
+        {
+            int nr = 0;
+            int auxl = l;
+            while(auxl<6 && mat[auxl,c]==val)
+            {
+                auxl++;
+                nr++;
+            }
+            auxl = l - 1;
+            while(auxl>=0 && mat[auxl,c]==val)
+            {
+                auxl--;
+                nr++;
+            }
+            if (nr >= 4)
+                return true;
+            return false;
+        }
+
+        public void reset_game()
+        {
+            for(int i = 0; i < 6; ++i)
+            {
+                for (int j = 0; j < 7; ++j)
+                    mat[i, j] = 0;
+            }
+        }
+        private bool verificare4(int l, int c, int val)
+        {
+            return verificare_linie(l,c, val) || verificare_coloana(l,c, val);
+        }
+
+        public void desenare_cerc(int i, int j, Graphics e, int culoare)
+        {
+            if (culoare == 0)
+            {
+                e.FillEllipse(Brushes.White, 32 + 48*j, 32 + 48*i, 32, 32);
+           
+            }
+            else if (culoare==1)
+            {
+                e.FillEllipse(Brushes.Yellow, 32 + 48 * j, 32 + 48 * i, 32, 32);
+                this.mat[i, j] = 1;
+                if(verificare4(i,j,1))
+                {
+                    MessageBox.Show("Jucatorul 1 a castigat");
+                }
+            }
+            else if (culoare==2)
+            {
+                e.FillEllipse(Brushes.Red, 32 + 48 * j, 32 + 48 * i, 32, 32);
+                this.mat[i, j] = 2;
+                if (verificare4(i,j,2))
+                {
+                    MessageBox.Show("Jucatorul 2 a castigat");
+                }
+               
+            }
+        }
+    }
+}
